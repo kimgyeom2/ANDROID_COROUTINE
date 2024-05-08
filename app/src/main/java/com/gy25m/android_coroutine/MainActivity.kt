903a8922d617087fee7627bc9bc08576018124ef
kim gyeom <kimgyeom2@gmail.com>
2024-05-08 17:11:01 +0900

    Initial commit

---
+++ app/src/main/java/com/gy25m/android_coroutine/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.gy25m.android_coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+fun main() = runBlocking {
+    launch {
+        wait1000ms()
+        println("HELLO!")
+    }
+    println("World")
+}
+
+suspend fun wait1000ms(){
+    delay(1000L)
+}
